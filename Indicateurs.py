e0850c2c59fd358dbd845d0a3a3c8a5969c16e23
EdgarCourqueux <167227700+EdgarCourqueux@users.noreply.github.com>
2025-01-11 14:37:58 +0100

    Update Indicateurs.py

--- Indicateurs.py
+++ Indicateurs.py
@@ -9,6 +9,8 @@ import plotly.express as px
 import matplotlib.dates as mdates
 
 class Indicateurs:
+
+    ######################PERFORMANCE#########################################
     def performance(self, df, montant_initial, date_initiale, date_finale):
         # Convertir les colonnes de dates en format datetime sans spécifier de format explicite
         df['Date'] = pd.to_datetime(df['Date'])
@@ -47,62 +49,51 @@ class Indicateurs:
             "weekly_performance": df_resampled_weekly[['Adj Close', 'Weekly_Performance']],
             "monthly_performance": df_resampled_monthly[['Adj Close', 'Monthly_Performance']]
         }
-    def matrice_correlation(self, tickers_selectionnes, start_date="2010-01-01", end_date=None):
-        """
-        Calcule et affiche une matrice de corrélation pour les entreprises sélectionnées.
-        
-        Arguments:
-            tickers_selectionnes (list): Liste des tickers des entreprises.
-            start_date (str): Date de début pour les données (format 'YYYY-MM-DD').
-            end_date (str): Date de fin pour les données (par défaut : aujourd'hui).
-            
-        Retourne:
-            DataFrame: Matrice de corrélation entre les rendements journaliers des entreprises.
-        """
+    ######################Graphique#########################################
+    def matrice_correlation(tickers_selectionnes, start_date="2010-01-01", end_date=None):
         if end_date is None:
             end_date = datetime.now().strftime('%Y-%m-%d')
         
+        if not isinstance(tickers_selectionnes, list) or len(tickers_selectionnes) == 0:
+            raise ValueError("tickers_selectionnes doit être une liste non vide.")
+
         # Télécharger les données pour chaque ticker
-        data_dict = {
-            ticker: yf.download(ticker, start=start_date, end=end_date)['Adj Close']
-            for ticker in tickers_selectionnes
-        }
+        data_dict = {}
+        for ticker in tickers_selectionnes:
+            try:
+                data = yf.download(ticker, start=start_date, end=end_date)['Adj Close']
+                if not data.empty:
+                    data_dict[ticker] = data
+            except Exception as e:
+                print(f"Erreur lors du téléchargement des données pour {ticker}: {e}")
         
-        # Créer un DataFrame combiné avec les prix ajustés
+        # Vérifier si des données valides ont été collectées
+        if len(data_dict) == 0:
+            raise ValueError("Aucune donnée valide n'a été collectée pour les tickers sélectionnés.")
+
+        # Convertir les données en DataFrame
         df_prices = pd.DataFrame(data_dict)
         
-        # Calcul des rendements journaliers
+        # Calculer les rendements journaliers
         df_returns = df_prices.pct_change().dropna()
-        
-        # Calcul de la matrice de corrélation
+
+        # Calculer la matrice de corrélation
         correlation_matrix = df_returns.corr()
-        
-        # Afficher un graphique de la matrice de corrélation
-        plt.figure(figsize=(10, 8))
-        plt.matshow(correlation_matrix, cmap="coolwarm", fignum=1)
-        plt.colorbar()
-        plt.xticks(range(len(correlation_matrix.columns)), correlation_matrix.columns, rotation=45)
-        plt.yticks(range(len(correlation_matrix.columns)), correlation_matrix.columns)
-        plt.title("Matrice de Corrélation des Rendements Journaliers", pad=20)
-        plt.tight_layout()
-        plt.show()
-        
+
         return correlation_matrix
 
-    def afficher_graphique_interactif(self, tickers_selectionnes, montant_initial, date_investissement):
-        # Télécharger les données pour chaque ticker depuis 2010
+    def afficher_graphique_interactif(tickers_selectionnes, montant_initial, date_investissement, date_fin):
         data_dict = {
-            ticker: yf.download(ticker, start="2010-01-01", end=datetime.now().strftime('%Y-%m-%d')).reset_index()
+            ticker: yf.download(ticker, start="2010-01-01", end=date_fin.strftime('%Y-%m-%d')).reset_index()
             for ticker in tickers_selectionnes
         }
 
-        # Créer le graphique interactif pour l'évolution des prix (données complètes depuis 2010)
         fig_prices = go.Figure()
         for ticker, data in data_dict.items():
             data['Date'] = pd.to_datetime(data['Date'])
-            data = data.sort_values(by='Date')  # Ensure data is sorted by date
+            data = data.sort_values(by='Date')
 
-            # Ajouter la courbe du prix de clôture depuis 2010
+            # Tracé des lignes de prix de clôture
             fig_prices.add_trace(go.Scatter(
                 x=data['Date'],
                 y=data['Adj Close'],
@@ -110,52 +101,57 @@ class Indicateurs:
                 name=f"{ticker} (Prix de Clôture)"
             ))
 
-            # Extraire le prix à la date d'investissement pour le point
-            data_invest = data[data['Date'] >= date_investissement]
+            # Ajouter un point vert pour le début de l'investissement
+            data_invest = data[(data['Date'] >= date_investissement) & (data['Date'] <= date_fin)]
             if not data_invest.empty:
                 prix_investissement = data_invest['Adj Close'].iloc[0]
-                # Ajouter un point pour marquer la date d'investissement sans normalisation
                 fig_prices.add_trace(go.Scatter(
                     x=[date_investissement],
                     y=[prix_investissement],
                     mode='markers',
                     marker=dict(color='green', size=10),
-                    name=f"Investissement {ticker}"
+                    name=f"Début Investissement {ticker}"
+                ))
+
+                # Ajouter un point rouge pour la fin de l'investissement
+                prix_fin_investissement = data_invest['Adj Close'].iloc[-1]
+                fig_prices.add_trace(go.Scatter(
+                    x=[date_fin],
+                    y=[prix_fin_investissement],
+                    mode='markers',
+                    marker=dict(color='red', size=10),
+                    name=f"Fin Investissement {ticker}"
                 ))
 
         fig_prices.update_layout(
-            title="📈 Évolution des Prix des Tickers depuis 2010 avec Date d'Investissement",
+            title=f"📈 Évolution des Prix des Tickers ({date_investissement.date()} - {date_fin.date()})",
             xaxis_title="Date",
             yaxis_title="Prix de Clôture",
             hovermode="x unified"
         )
 
-        # Calculer la valeur du portefeuille pour chaque ticker et sa valeur totale (seulement depuis la date d'investissement)
+        # Normalisation des valeurs des tickers pour calculer la proportion du portefeuille
         portfolio_values = []
         for ticker, data in data_dict.items():
             data['Date'] = pd.to_datetime(data['Date'])
-            data_invest = data[data['Date'] >= date_investissement].copy()
+            data_invest = data[(data['Date'] >= date_investissement) & (data['Date'] <= date_fin)].copy()
             prix_investissement = data_invest['Adj Close'].iloc[0]
             montant_investissement_ticker = montant_initial / len(tickers_selectionnes) * (data_invest['Adj Close'] / prix_investissement)
             montant_investissement_ticker.index = data_invest['Date']
             montant_investissement_ticker = montant_investissement_ticker.rename(f"Montant_{ticker}")
             portfolio_values.append(montant_investissement_ticker)
 
-        # Concaténer les valeurs dans un DataFrame unique
         df_portfolio = pd.concat(portfolio_values, axis=1).ffill()
         df_portfolio['Montant_Total'] = df_portfolio.sum(axis=1)
 
-        # Calculer les proportions
         for ticker in tickers_selectionnes:
             df_portfolio[f'{ticker}_Proportion'] = (df_portfolio[f"Montant_{ticker}"] / df_portfolio['Montant_Total']) * 100
-        # Deuxième figure : Valeur du portefeuille normalisée pour superposition avec double axe y
+
         fig, ax1 = plt.subplots(figsize=(10, 8))
 
-        # Axe de gauche : Courbes des entreprises normalisées
         normalised_values = {}
         for ticker in tickers_selectionnes:
             if f"Montant_{ticker}" in df_portfolio:
-                # Normaliser pour que chaque courbe commence à 0
                 normalised_values[ticker] = (
                     df_portfolio[f"Montant_{ticker}"] - df_portfolio[f"Montant_{ticker}"].iloc[0]
                 )
@@ -165,15 +161,11 @@ class Indicateurs:
                     label=f"Valeur normalisée de {ticker}",
                     linestyle="--"
                 )
-            else:
-                print(f"Données pour {ticker} manquantes dans df_portfolio")
 
         ax1.set_xlabel("Date")
         ax1.set_ylabel("Valeur Normalisée des Entreprises (€)")
-        ax1.tick_params(axis='y')
         ax1.grid(True)
 
-        # Axe de droite : Courbe du portefeuille total (échelle ajustée)
         ax2 = ax1.twinx()
         df_portfolio["Montant_Total_Normalise"] = (
             df_portfolio["Montant_Total"] - df_portfolio["Montant_Total"].iloc[0]
@@ -188,32 +180,26 @@ class Indicateurs:
         ax2.set_ylabel("Valeur Normalisée du Portefeuille (€)", color="black")
         ax2.tick_params(axis='y', labelcolor="black")
 
-        # Ajouter une légende combinée pour les deux axes
         lines_1, labels_1 = ax1.get_legend_handles_labels()
         lines_2, labels_2 = ax2.get_legend_handles_labels()
         ax1.legend(lines_1 + lines_2, labels_1 + labels_2, loc="upper left")
 
-        # Configurer le format de date pour l'axe x
         ax1.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
         ax1.xaxis.set_major_locator(mdates.AutoDateLocator())
         fig.autofmt_xdate()
 
-        # Ajouter un titre
         fig.suptitle(
-            f"Évolution Normalisée de la Valeur du Portefeuille depuis {date_investissement.date()} (initialement {montant_initial} €)",
+            f"Évolution Normalisée de la Valeur du Portefeuille ({date_investissement.date()} - {date_fin.date()})",
             fontsize=14
         )
 
-        # Ajuster l'affichage et sauvegarder
         fig.tight_layout()
-        fig.subplots_adjust(top=0.9)  # Ajuster pour ne pas couper le titre
+        fig.subplots_adjust(top=0.9)
         plt.savefig("graph_multi_tickers.png")
         plt.show()
 
-
-        # Troisième graphique : Proportions des entreprises (aire empilée)
         plt.figure(figsize=(12, 8))
-        bottom = pd.Series(0, index=df_portfolio.index)  # Pour construire l'aire empilée
+        bottom = pd.Series(0, index=df_portfolio.index)
 
         for ticker in tickers_selectionnes:
             if f"{ticker}_Proportion" in df_portfolio.columns:
@@ -226,31 +212,16 @@ class Indicateurs:
                 )
                 bottom += df_portfolio[f"{ticker}_Proportion"]
 
-        # Tracer les lignes horizontales pointillées
-        num_tickers = len(tickers_selectionnes)
-        for i in range(1, num_tickers):  # Éviter les lignes à 0 % et 100 %
-            proportion_line = (100 / num_tickers) * i
-            plt.axhline(
-                y=proportion_line,
-                color="gray",
-                linestyle="--",
-                linewidth=0.8,
-                label=f"Ligne {proportion_line:.1f} %"
-            )
-
-        # Ajouter des détails au graphique
         plt.title("Proportions des Entreprises dans le Portefeuille au Fil du Temps (Aire Empilée)", fontsize=16)
         plt.xlabel("Date", fontsize=14)
         plt.ylabel("Proportion (%)", fontsize=14)
         plt.legend(title="Entreprises", fontsize=12)
         plt.grid(True)
 
-        # Configurer les dates
         plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
         plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator())
         plt.gcf().autofmt_xdate()
 
-        # Ajuster l'affichage et sauvegarder
         plt.tight_layout()
         plt.savefig("graph_proportions_tickers.png")
         plt.show()
@@ -258,7 +229,93 @@ class Indicateurs:
         return fig_prices
 
 
+    def evolution_valeur_portefeuille(dates, valeurs_portefeuille):
+        """
+        Graphique de l'évolution de la valeur du portefeuille au fil du temps.
 
+        :param dates: Liste des dates correspondant aux périodes.
+        :param valeurs_portefeuille: Liste des valeurs du portefeuille à ces dates.
+        :return: Figure Plotly à afficher.
+        """
+        # Vérifier et aligner les longueurs des données
+        min_length = min(len(dates), len(valeurs_portefeuille))
+        dates = dates[:min_length]
+        valeurs_portefeuille = valeurs_portefeuille[:min_length]
+
+        # Créer le graphique
+        fig = px.line(
+            x=dates,
+            y=valeurs_portefeuille,
+            title="Évolution de la Valeur du Portefeuille",
+            labels={"x": "Date", "y": "Valeur du Portefeuille (€)"},
+        )
+        fig.update_traces(line=dict(width=2))
+        return fig
+
+
+
+    def evolution_rendements_actifs(dates, rendements_cumules):
+        """
+        Graphique de l'évolution des rendements cumulés des actifs sélectionnés.
+
+        :param dates: Liste des dates correspondant aux périodes.
+        :param rendements_cumules: DataFrame où chaque colonne représente un actif, avec les rendements cumulés.
+        :return: Figure Plotly à afficher.
+        """
+        rendements_cumules['Date'] = dates
+        df = rendements_cumules.melt(id_vars="Date", var_name="Actif", value_name="Rendement Cumulé (%)")
+
+        fig = px.line(
+            df,
+            x="Date",
+            y="Rendement Cumulé (%)",
+            color="Actif",
+            title="Évolution des Rendements Cumulés des Actifs",
+        )
+        fig.update_traces(line=dict(width=2))
+        return fig
+    
+    def afficher_repartition_dynamique(self, repartition):
+        """
+        Génère un graphique interactif de la répartition du portefeuille au fil du temps.
+
+        :param repartition: DataFrame avec les colonnes représentant les actifs et les lignes représentant les périodes.
+        """
+        if repartition.empty:
+            print("Aucune donnée disponible pour la répartition.")
+            return
+
+        # Remplacer les valeurs NaN par 0 pour les calculs
+        repartition = repartition.fillna(0)
+
+        # Préparer les données pour le graphique
+        repartition.reset_index(inplace=True)
+        repartition = repartition.melt(id_vars="index", var_name="Actif", value_name="Proportion")
+        repartition.rename(columns={"index": "Date"}, inplace=True)
+
+        # Créer le graphique d'aire empilée
+        fig = px.area(
+            repartition,
+            x="Date",
+            y="Proportion",
+            color="Actif",
+            title="Répartition Dynamique du Portefeuille au Fil du Temps",
+            labels={"Proportion": "Proportion (%)", "Date": "Date", "Actif": "Actifs"}
+        )
+        fig.update_traces(mode="lines")
+        fig.update_layout(
+            xaxis_title="Date",
+            yaxis_title="Proportion (%)",
+            hovermode="x unified"
+        )
+
+        return fig
+
+
+    
+
+
+    ######################Indicateurs#########################################
     def volatilite_historique(self, df, lambda_ewma=0.94):
         df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
         df = df.sort_values(by='Date')
